f1f1cfbd0211cb4b1bd7d01d7f01ccd72156f5ed
woosungkim0123 <sibu2005@naver.com>
2022-08-28 20:32:54 +0900

    네스트에서 controller, service, module단 추가 하는법 및 의존성 주입 개념 정도만. ps. 의존성 주입이 왜 필요한지는 얼추 알겠지만 아직 조금 더 알아봐야 할듯

---
+++ src/dms/dms.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, Param, Post, Query } from '@nestjs/common';
+
+@Controller('api/workspaces/:url/dms')
+export class DmsController {
+  @Get(':id/chats')
+  getChat(@Query() query, @Param() param) {
+    console.log(query.perPage, query.page);
+  }
+
+  @Post(':id/chats')
+  postChat() {}
+}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import morgan from 'morgan';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+  app.use(morgan('dev'));
   await app.listen(port);
   console.log(`listening on port ${port}`);
   if (module.hot) {
